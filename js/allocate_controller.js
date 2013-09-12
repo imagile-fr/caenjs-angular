de18bd4b4bc0f818970c76262e5b69c48e244e88
Pierre-Emmanuel Fringant <pierre.fringant@gmail.com>
2013-09-12 17:30:14 +0200

    Step 4 : Display workshop seats

--- js/allocate_controller.js
+++ js/allocate_controller.js
@@ -1,16 +1,20 @@
 var AllocateController = function($scope) {
   $scope.workshops = [
     {
-      name: "Atelier 1"
+      name: "Atelier 1",
+      seats: 5
     },
     {
-      name: "Atelier 2"
+      name: "Atelier 2",
+      seats: 2
     },
     {
-      name: "Atelier 3"
+      name: "Atelier 3",
+      seats: 8
     },
     {
-      name: "Atelier 4"
+      name: "Atelier 4",
+      seats: 4
     },
   ];
 }
