416dfb04112d278ae0cd953394cb4dab167288c4
Pierre-Emmanuel Fringant <pierre.fringant@gmail.com>
2013-09-12 17:24:28 +0200

    Step 3 : Repeat workshop blocks

---
+++ js/allocate_controller.js
@@ -0,0 +1,16 @@
+var AllocateController = function($scope) {
+  $scope.workshops = [
+    {
+      name: "Atelier 1"
+    },
+    {
+      name: "Atelier 2"
+    },
+    {
+      name: "Atelier 3"
+    },
+    {
+      name: "Atelier 4"
+    },
+  ];
+}
